bb256fbc2e617784cd9b6eca247b01f67589e8eb
haikalazhar197 <haikalazhar197@gmail.com>
2020-08-25 17:31:05 +0800

    Done lesson 3

--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,18 @@
-var character = "Haikal";
-var age = 23;
-var isBlackBelt = false;
+let character = "Haikal";
+let age = 23;
+let isBlackBelt = false;
+
 // character = 10;
 character = "Azhar";
+
 // age = "Marha";
 age = 25;
+
 // isBlackBelt = 'Yes';
 isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
 };
+
 console.log(circ(20));
