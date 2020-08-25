c85a3078a2581c69e2b0c75cf9d10b08966231f8
haikalazhar197 <haikalazhar197@gmail.com>
2020-08-25 18:04:18 +0800

    Done lesson 4

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,28 @@
-var character = "Haikal";
-var age = 23;
-var isBlackBelt = false;
-// character = 10;
-character = "Azhar";
-// age = "Marha";
-age = 25;
-// isBlackBelt = 'Yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+//arrays
+var names = ["Haikal", "Maya", "Sabrina"];
+names.push("AZHAR");
+// names.push(23);
+// names[0] = 4;
+var numbers = [1, 2, 3, 4];
+numbers.push(25);
+// numbers.push("hello");
+// numbers[1] = "hello";
+var mixed = ["haikal", 4, "hello", 8, 9];
+mixed.push("Erysha");
+mixed.push(45);
+mixed[2] = "Azhar";
+//objects
+var person = {
+    name: "Haikal",
+    belt: "black",
+    age: 23
 };
-console.log(circ(20));
+person.age = 35;
+person.name = "Maya";
+// person.age = "30";
+// person.skills = ['fighting', 'sneaking'];
+person = {
+    name: "Maya",
+    belt: "white",
+    age: 40
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,36 @@
-let character = "Haikal";
-let age = 23;
-let isBlackBelt = false;
+//arrays
+let names = ["Haikal", "Maya", "Sabrina"];
 
-// character = 10;
-character = "Azhar";
+names.push("AZHAR");
+// names.push(23);
+// names[0] = 4;
 
-// age = "Marha";
-age = 25;
+let numbers = [1, 2, 3, 4];
 
-// isBlackBelt = 'Yes';
-isBlackBelt = true;
+numbers.push(25);
+// numbers.push("hello");
+// numbers[1] = "hello";
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+let mixed = ["haikal", 4, "hello", 8, 9];
+mixed.push("Erysha");
+mixed.push(45);
+mixed[2] = "Azhar";
+
+//objects
+
+let person = {
+  name: "Haikal",
+  belt: "black",
+  age: 23,
 };
 
-console.log(circ(20));
+person.age = 35;
+person.name = "Maya";
+// person.age = "30";
+// person.skills = ['fighting', 'sneaking'];
+
+person = {
+  name: "Maya",
+  belt: "white",
+  age: 40,
+};
