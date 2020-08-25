82a4421d6b90d772b534dee62779c7e49908e7d5
haikalazhar197 <haikalazhar197@gmail.com>
2020-08-26 00:38:38 +0800

    Done lesson 10

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,10 +1,22 @@
 "use strict";
-var logDetails = function (uid, item) {
-    console.log(item + " has uid of " + uid);
+// let greet: Function;
+// Example 1
+var greet;
+greet = function (name, greeting) {
+    console.log(name + " says " + greeting);
 };
-var greet = function (user) {
-    console.log(user.name + " says hello");
+// Example 2
+var calc;
+calc = function (num1, num2, action) {
+    if (action === "add") {
+        return num1 + num2;
+    }
+    else {
+        return num1 - num2;
+    }
 };
-var greetAgain = function (user) {
-    console.log(user.name + " says hello");
+// Example 3
+var logDetails;
+logDetails = function (ninja) {
+    console.log(ninja.name + " is " + ninja.age + " years old");
 };
