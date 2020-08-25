f84cea2c5f2cd00b0631aefcd2212d6259a94c82
haikalazhar197 <haikalazhar197@gmail.com>
2020-08-26 00:12:58 +0800

    Done lesson 8

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("test");
+var greet;
+// greet = "haikal";
+greet = function () {
+    console.log("hello again");
+};
+//optional parameters at the end
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(2, 3, "20");
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,21 @@
-console.log("test");
+let greet: Function;
+
+// greet = "haikal";
+
+greet = () => {
+  console.log("hello again");
+};
+
+//optional parameters at the end
+const add = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(2, 3, "20");
+
+const minus = (a: number, b: number): number => {
+  return a + b;
+};
+
+let result = minus(10, 7);
