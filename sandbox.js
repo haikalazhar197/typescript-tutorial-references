49b33f62e00b87314148adc82012c467b1548607
haikalazhar197 <haikalazhar197@gmail.com>
2020-08-25 17:21:49 +0800

    Done lesson 2

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var chracter = "Haikal";
+console.log(chracter);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const chracter = "Haikal";
+
+console.log(chracter);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+inputs.forEach((input) => {
+  console.log(input);
+});
