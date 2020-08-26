2508b5f2ae75b3d14e11fba9d2bf84cef311f205
haikalazhar197 <haikalazhar197@gmail.com>
2020-08-26 23:52:11 +0800

    Done lesson 17 - rendering HTML

--- public/app.js
+++ public/app.js
@@ -1,21 +1,6 @@
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/Payment.js";
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice("Haikal", "Web Work", 250);
-// docTwo = new Payment("Maya", "Plumbing", 300);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const invOne = new Invoice("Sabrina", "Work on Something", 40);
-// const invTwo = new Invoice("Maya", "Work on Something", 300);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// invoices.forEach((invoice) => {
-//   console.log(invoice.client, invoice.amount, invoice.format());
-// });
+import { ListTemplate } from "./classes/ListTemplate.js";
 const form = document.querySelector(".new-item-form");
 // console.log(form.children);
 // inputs
@@ -23,6 +8,9 @@ const type = document.querySelector("#type");
 const toFrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// Lsit template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -32,5 +20,6 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    // console.log(doc);
+    list.render(doc, type.value, "end");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,7 @@
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./Interfaces/HasFormatter.js";
-
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice("Haikal", "Web Work", 250);
-// docTwo = new Payment("Maya", "Plumbing", 300);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-// const invOne = new Invoice("Sabrina", "Work on Something", 40);
-// const invTwo = new Invoice("Maya", "Work on Something", 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// invoices.forEach((invoice) => {
-//   console.log(invoice.client, invoice.amount, invoice.format());
-// });
+import { ListTemplate } from "./classes/ListTemplate.js";
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 // console.log(form.children);
@@ -33,6 +11,10 @@ const type = document.querySelector("#type") as HTMLSelectElement;
 const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// Lsit template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
@@ -45,5 +27,7 @@ form.addEventListener("submit", (e: Event) => {
     doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  // console.log(doc);
+
+  list.render(doc, type.value, "end");
 });
